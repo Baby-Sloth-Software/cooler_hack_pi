b877f5ddf2b0b18831094599b9677c3f63b53cca
violetaria <violetaria@yahoo.com>
2016-04-17 11:44:01 -0400

    got python sending post requests

--- cooler.py
+++ cooler.py
@@ -1,5 +1,10 @@
 import smbus
 import time
+import json
+import urllib2
+import urllib
+import requests
+
 range = 200
 bus = smbus.SMBus(1)
 # I2C address 0x29
@@ -28,8 +33,19 @@ if ver == 0x44:
         print crgb
         time.sleep(.5)
         if clear < (original_clear - range) or clear > (original_clear + range):
-            print "something fucking changed\n"
+            input_data = { 'unit_id': '1',
+                           'c_value': int(round(clear, -2)),
+                           'r_value': int(round(red, -2)),
+                           'g_value': int(round(green, -2)),
+                           'b_value': int(round(blue, -2))
+                         }
+            url = "https://coolscandeluxe.herokuapp.com/datadump"
+            r = requests.post(url, data=json.dumps(input_data),
+                              headers={'Content-Type': 'application/json'})
+            response = r.text
+            print response
             time.sleep(2)
+            data = bus.read_i2c_block_data(0x29, 0)
             original_clear = clear = data[1] << 8 | data[0]
             original_red = data[3] << 8 | data[2]
             original_green = data[5] << 8 | data[4]
